cee9babd6208fd74ec9304e69a9369a7210acac8
Valentin Liévin <valentin.lievin@gmail.com>
2021-08-26 12:03:40 +0200

    experiments: updated and improved parameter overriding

--- fz_openqa/ops/training.py
+++ fz_openqa/ops/training.py
@@ -49,7 +49,7 @@ def train(config: DictConfig) -> Optional[float]:
 
     # Set seed for random number generators in pytorch, numpy and python.random
     if "seed" in config:
-        seed_everything(config.seed, workers=True)
+        seed_everything(config.base.seed, workers=True)
 
     # Init HuggingFace tokenizer
     log.info(f"Instantiating tokenizer <{config.tokenizer._target_}>")
