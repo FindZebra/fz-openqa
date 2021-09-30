7dfa328b074dd35d941252979f49511c485bf8d7
Valentin Liévin <valentin.lievin@gmail.com>
2021-09-30 10:40:48 +0200

    install scispacy models

---
+++ install_scispacy_models.sh
@@ -0,0 +1,4 @@
+poetry run pip install https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.4.0/en_core_sci_sm-0.4.0.tar.gz
+poetry run pip install https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.4.0/en_core_sci_md-0.4.0.tar.gz
+poetry run pip install https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.4.0/en_core_sci_lg-0.4.0.tar.gz
+poetry run pip install https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.4.0/en_ner_bc5cdr_md-0.4.0.tar.gz
