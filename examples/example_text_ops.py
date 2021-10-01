1f52710c5ff6281f77ebedace9235d191d1a11f9
Ida Riis Jensen <46669248+idariis@users.noreply.github.com>
2021-10-01 12:14:49 +0200

    testing text operations

--- examples/example_text_ops.py
+++ examples/example_text_ops.py
@@ -1,12 +1,17 @@
 import numpy as np
-import rich, json, os
+import rich, json, os, sys
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
 
 from fz_openqa.datamodules.corpus_dm import MedQaCorpusDataModule, FzCorpusDataModule
-from fz_openqa.utils.es_functions import ElasticSearch
+from fz_openqa.datamodules.meqa_dm import MedQaDataModule
 from fz_openqa.tokenizers.pretrained import init_pretrained_tokenizer
 
 tokenizer = init_pretrained_tokenizer(pretrained_model_name_or_path='bert-base-cased')
 
+# load the corpus object
 corpus = MedQaCorpusDataModule(tokenizer=tokenizer,
                                passage_length=200,
                                passage_stride=100,
@@ -16,5 +21,16 @@ corpus = MedQaCorpusDataModule(tokenizer=tokenizer,
                                verbose=False)
 corpus.prepare_data()
 corpus.setup()
+rich.print(corpus.dataset['document.text'])
 
-rich.print(corpus.dataset[0])
+# load the QA dataset
+#dm = MedQaDataModule(tokenizer=tokenizer,
+#                     num_proc=1,
+#                     use_subset=True,
+#                     verbose=True,
+#                     corpus=corpus,
+#                     n_documents=3)
+
+#dm.prepare_data()
+#dm.setup()
+#rich.print(dm.dataset[0])
--- fz_openqa/datamodules/pipes/text_ops.py
+++ fz_openqa/datamodules/pipes/text_ops.py
@@ -1,18 +1,30 @@
+import re
+
 from ...utils.datastruct import Batch
 from .base import Pipe
 
 
 class TextCleaner(Pipe):
     def __init__(
-        self, text_key: str, *, remove_breakline: bool = True, **kwargs
+        self,
+        text_key: str,
+        *,
+        remove_breakline: bool = True,
+        remove_ref: bool = True,
+        **kwargs
     ):
         super().__init__(**kwargs)
         self.text_key = text_key
         self.remove_breakline = remove_breakline
+        self.remove_ref = remove_ref
 
     def process_one(self, txt: str) -> str:
         if self.remove_breakline:
-            txt = txt.replace("\n", "")
+            txt = re.sub(r"[\n]", "", txt)
+
+        if self.remove_ref:
+            txt = re.sub(r"\u2003", " ", txt)
+
         return txt
 
     def __call__(self, batch: Batch) -> Batch:
