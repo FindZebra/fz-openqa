c2f2e4be85e131cfb7dcf63db9d14927ef298dca
Ida Riis Jensen <46669248+idariis@users.noreply.github.com>
2021-10-04 13:05:50 +0200

    deleting test file for dynamic ranking as it will be done in the exact match branch

--- tests/test_dyn_ranking.py
+++
@@ -1,6 +0,0 @@
-from unittest import TestCase
-from transformers import AutoTokenizer
-
-class TestDynamicRanking(TestCase):
-    def setUp(self) -> None:
-        tokenizer = AutoTokenizer.from_pretrained('bert-base-cased')
