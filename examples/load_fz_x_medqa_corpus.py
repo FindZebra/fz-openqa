5543a91e601ee503be7784403dd1fe642054b514
Valentin Liévin <valentin.lievin@gmail.com>
2021-10-15 17:26:21 +0200

    cleanup example

--- examples/load_fz_x_medqa_corpus.py
+++ examples/load_fz_x_medqa_corpus.py
@@ -1,10 +1,8 @@
 import datasets
-import rich
-from fz_openqa.datamodules.corpus_dm import FZxMedQaCorpusDataModule, MedQaCorpusDataModule, FzCorpusDataModule
-from fz_openqa.datamodules.index import ElasticSearchIndex
-from fz_openqa.datamodules.meqa_dm import MedQaDataModule
+
+from fz_openqa.datamodules.corpus_dm import FZxMedQaCorpusDataModule
 from fz_openqa.tokenizers.pretrained import init_pretrained_tokenizer
-from fz_openqa.utils.pretty import get_separator, pprint_batch
+from fz_openqa.utils.pretty import get_separator
 
 datasets.set_caching_enabled(False)
 
@@ -12,12 +10,12 @@ tokenizer = init_pretrained_tokenizer(
     pretrained_model_name_or_path='bert-base-cased')
 
 # load the corpus object
-corpus = FzCorpusDataModule(tokenizer=tokenizer,
-                            index=None,
-                            verbose=False,
-                            num_proc=4,
-                            use_subset=False,
-                            train_batch_size=10)
+corpus = FZxMedQaCorpusDataModule(tokenizer=tokenizer,
+                                  index=None,
+                                  verbose=False,
+                                  num_proc=4,
+                                  use_subset=False,
+                                  train_batch_size=10)
 
 # full: 1 020 817
 # MedQA: 230 180
