bdc89d40d1f65697a63f5af1ea52cb655a80fe4d
Valentin Liévin <valentin.lievin@gmail.com>
2021-10-30 12:39:06 +0200

    Sort documents with `document.is_positive`

--- fz_openqa/datamodules/pipelines/preprocessing/sort_documents.py
+++ fz_openqa/datamodules/pipelines/preprocessing/sort_documents.py
@@ -1,16 +1,34 @@
+from fz_openqa.datamodules.pipes import DropKeys
+from fz_openqa.datamodules.pipes import Pipe
 from fz_openqa.datamodules.pipes import Sequential
 from fz_openqa.datamodules.pipes import Sort
 from fz_openqa.datamodules.pipes.control.filter_keys import KeyWithPrefix
 from fz_openqa.datamodules.pipes.nesting import Nested
+from fz_openqa.utils.datastruct import Batch
+
+
+class GenIsPositive(Pipe):
+    def __call__(self, batch: Batch, **kwargs) -> Batch:
+        batch["document.is_positive"] = [
+            x > 0 for x in batch["document.match_score"]
+        ]
+        return batch
 
 
 class SortDocuments(Sequential):
     def __init__(self):
         super().__init__(
             Nested(
-                Sort(
-                    keys=["document.match_score", "document.retrieval_score"],
-                    reversed=True,
+                Sequential(
+                    GenIsPositive(),
+                    Sort(
+                        keys=[
+                            "document.is_positive",
+                            "document.retrieval_score",
+                        ],
+                        reversed=True,
+                    ),
+                    DropKeys(["document.is_positive"]),
                 ),
                 filter=KeyWithPrefix("document."),
             ),
