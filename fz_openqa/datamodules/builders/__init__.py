7d716ffc512ef07492ccc0fbe2a566ee770cfab1
Valentin Liévin <valentin.lievin@gmail.com>
2021-11-03 10:36:22 +0100

    corrected `FzCorpusBuilder` name

--- fz_openqa/datamodules/builders/__init__.py
+++ fz_openqa/datamodules/builders/__init__.py
@@ -1,6 +1,6 @@
 from .base import DatasetBuilder
 from .corpus import CorpusBuilder
-from .corpus import FzCorpusCorpusBuilder
+from .corpus import FzCorpusBuilder
 from .corpus import FZxMedQaCorpusBuilder
 from .corpus import MedQaCorpusBuilder
 from .hf_dataset import HfDatasetBuilder
