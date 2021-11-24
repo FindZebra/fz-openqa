96d6eb1e8c1ab5ef0fb153d158effb9cc677adad
Ida <s161777@student.dtu.dk>
2021-11-24 14:30:02 +0100

    colbert index ready for test

--- fz_openqa/datamodules/index/colbert.py
+++ fz_openqa/datamodules/index/colbert.py
@@ -18,7 +18,7 @@ from fz_openqa.datamodules.index.dense import FaissIndex
 from fz_openqa.datamodules.index.search_result import SearchResult
 from fz_openqa.utils.datastruct import Batch
 
-logger = logging.getLogger(__name__)
+log = logging.getLogger(__name__)
 
 DEF_LOADER_KWARGS = {"batch_size": 10, "num_workers": 2, "pin_memory": True}
 DEFAULT_FAISS_KWARGS = {
@@ -32,9 +32,7 @@ DEFAULT_FAISS_KWARGS = {
 class ColbertIndex(FaissIndex):
     def __init__(self, dataset: Dataset, **kwargs):
         """
-        todo: @idariis : this needs to be adapted for Colbert
-        the init should mostly reuse the one from the parent class (FaissIndex)
-        you might need to init the token index (to store the index of the original document)
+        Reused init from parent class (FaissIndex)
         """
 
         # call the super: build the index
@@ -42,40 +40,41 @@ class ColbertIndex(FaissIndex):
 
     def _init_index(self, batch):
         """
-        Initialize the index
-         # todo: @idariis : this needs to be adapted for Colbert
+        Initialize the index and train on first batch of data
+
+        Parameters
+        ----------
+        vectors
+            The dataset to index.
+        metric_type
+            Distance metric, most common is METRIC.L2
+        n_list
+            The number of cells (partitions). Typical values is sqrt(N)
+        m
+            The number of sub-vectors. Typically this is 8, 16, 32, etc.
+        n_bits
+            Bits per subvector. Typically 8, so that each sub-vector is encoded by 1 byte
+        dim
+            The dimension of the input vectors
+
         """
         vectors = batch[self.vectors_column_name]
         assert len(vectors.shape) == 2
         metric_type = self.faiss_args["metric_type"]
-        n_list = self.faiss_args["n_list"]
+        n_list = self.faiss_args["n_list"], "n should be sqrt(len(dataset))"
         m = self.faiss_args["m"]
         n_bits = self.faiss_args["n_bits"]
-        dim = vectors.shape[-1]
-        assert dim % m == 0, "m must be a divisor of dim"
+        self.dim = vectors.shape[-1]
+        assert self.dim % m == 0, "m must be a divisor of dim"
 
-        quantizer = faiss.IndexFlatL2(dim)
-        self._index = faiss.IndexIVFPQ(quantizer, dim, n_list, m, n_bits, metric_type)
-
-    def _train(self, vector):
-        """
-        Train faiss index on data
-
-        Parameters
-        ----------
-        vector
-            vector from batch
-
-        """
-        self._index.train(vector)
+        quantizer = faiss.IndexFlatL2(self.dim)
+        self._index = faiss.IndexIVFPQ(quantizer, self.dim, n_list, m, n_bits, metric_type)
+        self._index.train(vectors)
         assert self._index.is_trained is True, "Index is not trained"
-        print("Done training!")
+        log.info(f"Index is trained: {self._index.is_trained}")
 
     def _add_batch_to_index(self, batch: Batch, dtype=np.float32):
-        """
-        Add one batch of data to the index
-         # todo: @idariis : this needs to be adapted for Colbert
-        """
+        """ Add one batch of data to the index """
         # check indexes
         indexes = batch[self.index_key]
         msg = f"Indexes are not contiguous (i.e. 1, 2, 3, 4),\nindexes={indexes}"
@@ -86,19 +85,18 @@ class ColbertIndex(FaissIndex):
         )
         assert self._index.ntotal == indexes[0], msg
 
-        # train index and add vector to index
+        # add vector to index
         vector: np.ndarray = batch[self.vectors_column_name]
         assert isinstance(vector, np.ndarray), f"vector {type(vector)} is not a numpy array"
         assert len(vector.shape) == 2, f"{vector} is not a 2D array"
         vector = vector.astype(dtype)
-        self._train(vector)
-        if self._index.is_trained is True:
-            self._index.add(vector)
-            # store token index to original document
-            self.tok2doc = []
-            for idx in vector["document.row_idx"]:
-                ids = np.linspace(idx, idx, num=198, dtype="int32").tolist()
-                self.tok2doc.extend(ids)
+        self._index.add(vector)
+
+        # store token index to original document
+        self.tok2doc = []
+        for idx in vector["document.row_idx"]:
+            ids = np.linspace(idx, idx, num=self.dim, dtype="int32").tolist()
+            self.tok2doc.extend(ids)
         rich.print(f"[red]Total number of indices: {self._index.ntotal}")
 
     def search(
@@ -111,15 +109,18 @@ class ColbertIndex(FaissIndex):
         """
         Search the index using the `query` and
         return the index of the results within the original dataset.
-        # todo: @idariis : this needs to be adapted for Colbert
+
+        Note
+        ----
+        Scores are distances, i.e. the smaller the better
         """
-        query = self.process(query)
+        query = self.predict_queries(query)
         query = self.postprocess(query)
         vectors = query[self.vectors_column_name]
         scores, indices = self._index.search(vectors, k)
 
-        indices_flat = [i for sublist in indices for i in sublist]
-        doc_indices = [self.tok2doc[index] for index in indices_flat]
-        doc_indices = set(doc_indices)
+        doc_indices = set(self.tok2doc[index] for index in indices.flatten("C"))
+
+        # todo: apply MaxSim to filter related documents further
 
         return SearchResult(score=scores, index=doc_indices)
