1ecf42e4a4dd7dbee082f3bd67e98fc0a5d7fd85
Ida <s161777@student.dtu.dk>
2021-11-25 15:30:17 +0100

    deleting similarity scripts

--- fz_openqa/modeling/similarities/dot_product.py
+++
@@ -1,9 +0,0 @@
-import torch
-from torch import Tensor
-
-from .base import GolbalSimilarity
-
-
-class DotProduct(GolbalSimilarity):
-    def vec_similarity(self, x: Tensor, y: Tensor) -> Tensor:
-        return torch.einsum("nh, mh -> nm", x, y)
