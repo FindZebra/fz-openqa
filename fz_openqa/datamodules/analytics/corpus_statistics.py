cdb839595918b58f896f5beaf8fdf4bb6f7775cb
Andreas Geert Motzfeldt <andreas@motzfeldt.dk>
2021-11-26 15:01:55 +0100

    report for statistics for a given corpus

---
+++ fz_openqa/datamodules/analytics/corpus_statistics.py
@@ -0,0 +1,27 @@
+from typing import Dict
+from typing import List
+
+import rich
+from datasets import Dataset
+
+from .base import Analytic
+
+
+class ReportCorpusStatistics(Analytic):
+    """Count the number of documents, tokens, and vocab size for a given corpus"""
+
+    requires_columns: List[str] = ["document.text"]
+    output_file_name: str = "corpus_statistics.json"
+
+    def process_dataset_split(self, dset: Dataset) -> Dict:
+        """
+        Report on a specific split of the dataset.
+        """
+        documents = dset["document.text"]
+        tokens = [token for doc in documents for token in doc.split()]
+        vocab = set(tokens)
+        return {
+            "paragraphs": len(documents),
+            "tokens": len(tokens),
+            "vocab": len(vocab),
+        }
