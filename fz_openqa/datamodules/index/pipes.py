64dc7dbf752c2899726e26227f8bc220cce63344
Valentin Liévin <valentin.lievin@gmail.com>
2021-12-09 10:40:55 +0100

    cleanup

--- fz_openqa/datamodules/index/pipes.py
+++ fz_openqa/datamodules/index/pipes.py
@@ -1,9 +1,6 @@
-from copy import deepcopy
-from dataclasses import dataclass
 from typing import List
 from typing import Optional
 
-import dill
 from datasets import Dataset
 
 from fz_openqa.datamodules.index.base import Index
@@ -14,44 +11,6 @@ from fz_openqa.datamodules.pipes.collate import Collate
 from fz_openqa.datamodules.pipes.control.condition import In
 from fz_openqa.utils.array import concat_arrays
 from fz_openqa.utils.datastruct import Batch
-
-
-@dataclass
-class SearchResult:
-    score: List[List[float]]
-    index: List[List[int]]
-    tokens: List[List[str]]
-
-
-class FakeIndex:
-    """A small class to test Search corpus without using a proper index"""
-
-    index_name = "<name>"
-
-    def search(self, *, query: Batch, k: int, **kwargs) -> SearchResult:
-        values = query["question.text"]
-        return SearchResult(
-            index=[[0 for _ in range(k)] for _ in values],
-            score=[[1.0 for _ in range(k)] for _ in values],
-        )
-
-    def dill_inspect(self) -> bool:
-        """check if the module can be pickled."""
-        return dill.pickles(self)
-
-
-class FakeDataset:
-    """A small class to test Search corpus without using a proper index"""
-
-    def __init__(self):
-        self.data = {"document.text": "<text>", "document.row_idx": 0}
-
-    def __getitem__(self, idx):
-        """check if the module can be pickled."""
-        if isinstance(idx, str):
-            return [deepcopy(self.data)]
-        else:
-            return deepcopy(self.data)
 
 
 class SearchCorpus(ApplyAsFlatten):
