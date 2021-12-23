759566c7bc849c16d1a53abb409cba28aea4d952
Andreas Geert Motzfeldt <andreas@motzfeldt.dk>
2021-12-23 14:44:34 +0100

    plot using seaborn instead of plotly

--- fz_openqa/datamodules/analytics/plot_match_triggers.py
+++ fz_openqa/datamodules/analytics/plot_match_triggers.py
@@ -1,4 +1,4 @@
-from __future__ import annotations
+from __future__ import annotations  # noqa: F407
 
 import functools
 import itertools
@@ -6,8 +6,10 @@ from collections import Counter
 from typing import Dict
 from typing import List
 
+import pandas as pd
 import plotly.express as px
 import rich
+import seaborn as sns
 from datasets import Dataset
 
 from .base import Analytic
@@ -18,7 +20,7 @@ class PlotTopMatchTriggers(Analytic):
     """ Plot the top 20 triggers for matched documents"""
 
     requires_columns = ["document.retrieval_score", "document.match_score"]
-    output_file_name = "top_match_triggers.html"
+    output_file_name = "top_match_triggers"
 
     def __init__(self, *args, topn: int = 100, **kwargs):
         super(PlotTopMatchTriggers, self).__init__(*args, **kwargs)
@@ -44,7 +46,6 @@ class PlotTopMatchTriggers(Analytic):
         Plot most common values.
         """
 
-        # create figure
         fig = px.bar(y=counts, x=labels, text=counts)
         fig.update_traces(textposition="outside", marker_color="rgb(102, 178, 255)")
         fig.update_yaxes(ticklabelposition="inside top", title="Count")
