6092785a4f82f28b8f126390ab69a0ceff366983
Ida <s161777@student.dtu.dk>
2022-01-03 11:46:42 +0100

    pubmedqa builder

---
+++ examples/load_pubmedqa.py
@@ -0,0 +1,49 @@
+import logging
+import os
+from pathlib import Path
+
+import datasets
+import hydra
+import rich
+from omegaconf import DictConfig
+from rich.logging import RichHandler
+
+import fz_openqa
+from fz_openqa import configs
+from fz_openqa.datamodules.builders.pubmedqa import PubMedQaBuilder
+from fz_openqa.datamodules.datamodule import DataModule
+from fz_openqa.tokenizers.pretrained import init_pretrained_tokenizer
+
+
+@hydra.main(
+    config_path=str(Path(configs.__file__).parent),
+    config_name="script_config.yaml",
+)
+def run(config: DictConfig) -> None:
+    os.environ["TOKENIZERS_PARALLELISM"] = "false"
+    datasets.set_caching_enabled(True)
+
+    # define the default cache location
+    default_cache_dir = Path(fz_openqa.__file__).parent.parent / "cache"
+
+    # initialize the tokenizer
+    tokenizer = init_pretrained_tokenizer(pretrained_model_name_or_path="bert-base-cased")
+
+    # initialize the data module
+    builder = PubMedQaBuilder(
+        tokenizer=tokenizer,
+        use_subset=config.get("use_subset", True),
+        cache_dir=config.get("cache_dir", default_cache_dir),
+        num_proc=2,
+    )
+    dm = DataModule(builder=builder)
+    dm.prepare_data()
+    dm.setup()
+    dm.display_samples()
+
+    # access dataset
+    rich.print(dm.dataset)
+
+
+if __name__ == "__main__":
+    run()
--- fz_openqa/datamodules/builders/pubmedqa.py
+++ fz_openqa/datamodules/builders/pubmedqa.py
@@ -6,22 +6,43 @@ from datasets.dataset_dict import DatasetDict
 from datasets.splits import Split
 
 from .hf_dataset import HfDatasetBuilder
+from fz_openqa.datamodules.pipelines.preprocessing.text import FormatAndTokenize
+from fz_openqa.datamodules.pipes.meta import Parallel
 from fz_openqa.datamodules.utils.typing import HfDataset
+from fz_openqa.tokenizers.static import ANS_TOKEN
+from fz_openqa.tokenizers.static import QUERY_TOKEN
 
 
 class PubMedQaBuilder(HfDatasetBuilder):
     # HuggingFace dataset id
     dset_script_path_or_id = "pqa_labeled"
 
+    # attributes to be converted to tensors in preprocessing
     pt_attributes = [
+        "answer.label",
+        "answer.input_ids",
+        "answer.attention_mask",
         "question.input_ids",
         "question.attention_mask",
         "pubid",
     ]
 
-    def __init__(self, *args, split: Split, **kwargs):
+    # number of data points per subset train/val/test
+    subset_size = [1000, 100, 100]
+
+    # output columns
+    column_names = [
+        "answer.label",
+        "answer.input_ids",
+        "answer.attention_mask",
+        "question.text",
+        "question.input_ids",
+        "question.attention_mask",
+    ]
+
+    def __init__(self, *args, n_query_tokens: int = 1, **kwargs):
         super(PubMedQaBuilder, self).__init__(*args, **kwargs)
-        self.split = str(split)
+        self.n_query_tokens = n_query_tokens
 
     def load_base_dataset(self) -> DatasetDict:
         dataset = load_dataset("pubmed_qa", self.dset_script_path_or_id)  # todo: make more dynamic
@@ -33,3 +54,40 @@ class PubMedQaBuilder(HfDatasetBuilder):
         dataset["test"] = dataset_eval["test"]
 
         return dataset
+
+    def preprocess_dataset(self, dataset: HfDataset) -> HfDataset:
+
+        # Tokenize question and answer
+        if self.tokenizer:
+            dataset = dataset.map(
+                Parallel(self.get_question_tokenizer_pipe(), self.get_answer_tokenizer_pipe()),
+                batched=True,
+                num_proc=self.num_proc,
+                desc="Tokenization of questions and answers",
+            )
+
+        return dataset
+
+    def get_answer_tokenizer_pipe(self):
+        return FormatAndTokenize(
+            prefix="answer.",
+            text_formatter=None,
+            tokenizer=self.tokenizer,
+            max_length=None,
+            add_encoding_tokens=self.add_encoding_tokens,
+            spec_tokens=ANS_TOKEN,
+            shape=None,
+        )
+
+    def get_question_tokenizer_pipe(self):
+        """create a Pipe to tokenize the questions."""
+        return FormatAndTokenize(
+            prefix="question.",
+            text_formatter=None,
+            tokenizer=self.tokenizer,
+            max_length=None,
+            add_encoding_tokens=self.add_encoding_tokens,
+            add_special_tokens=self.add_special_tokens,
+            spec_tokens=self.n_query_tokens * [QUERY_TOKEN],
+            shape=None,
+        )
