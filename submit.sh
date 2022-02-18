79e1b821167414708e3d788a598e32a1d507acc4
Valentin Liévin <valentin.lievin@gmail.com>
2022-02-18 12:27:57 +0100

    startup elastic search with slurm

--- submit.sh
+++ submit.sh
@@ -2,24 +2,32 @@
 #SBATCH --job-name=fz-openqa
 #SBATCH --output=./slurm/%j.out
 #SBATCH --ntasks=1 --cpus-per-task=16 --mem=64G
-#SBATCH -p gpu --gres=gpu:titanrtx:8
+#SBATCH -p gpu --gres=gpu:titanrtx:4
 #SBATCH --time=1-00:00:00
 
-#your script, in this case: write the hostname and the ids of the chosen gpus.
+# variables
+NAME="colbert-bayes-reinforce-v4.2.c-es-f15-k10-m100-diku-7"
+setup_with_model=false
+
+# display basic info
 hostname
 echo $CUDA_VISIBLE_DEVICES
 echo "======================"
 poetry run gpustat --debug
 echo "====== starting experiment ========="
-NAME="colbert-reinforce-v4.2.c-g.sch-zero-f5-k10-diku-6"
+
+# startup elastic search
+if [ "$setup_with_model" = false ]
+then
+    ./elasticsearch.sh &
+fi
+
+# run the model
 poetry run python run.py +experiment=option_retriever +environ=diku \
-  +setup_with_model=true \
+  +setup_with_model=${setup_with_model} \
   datamodule.n_documents=10 \
-  datamodule.train_batch_size=8 \
-  datamodule.eval_batch_size=32 \
-  trainer.accumulate_grad_batches=4 \
   logger.wandb.name=${NAME} \
   model.ema_decay=null \
   model.module.use_gate=false \
-  datamodule.dataset_update.freq=5 \
-  trainer.gpus=8
+  datamodule.dataset_update.freq=15 \
+  trainer.gpus=4
