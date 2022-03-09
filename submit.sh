546c488168a156710a4dbd8f77b90f99ad7c6929
Valentin Liévin <valentin.lievin@gmail.com>
2022-03-09 19:07:30 +0100

    catch utils.git exceptions + update main config

--- submit.sh
+++ submit.sh
@@ -2,11 +2,11 @@
 #SBATCH --job-name=fz-openqa
 #SBATCH --output=./slurm/%j.out
 #SBATCH --ntasks=1 --cpus-per-task=32 --mem=128G
-#SBATCH -p gpu --gres=gpu:titanrtx:8
+#SBATCH -p gpu --gres=gpu:titanrtx:4
 #SBATCH --time=1-00:00:00
 
 # variables
-NAME="xyt-DIKU-scaled-fxmatch-inbatch-v4.4.A-L350-9.3.1"
+NAME="xyt-DIKU-fxmatch-inbatch-v4.4.A-L350-9.3.1-attn-rop0.5"
 setup_with_model=false
 
 # display basic info
@@ -30,6 +30,7 @@ poetry run python run.py +experiment=option_retriever +environ=diku \
   trainer.precision=32 \
   datamodule.num_workers=12 \
   datamodule.builder.dataset_builder.max_length=350 \
+  model.bert.config.attention_probs_dropout_prob=0.5 \
   +setup_with_model=${setup_with_model} \
   +kill_es=true \
   logger.wandb.name=${NAME}
