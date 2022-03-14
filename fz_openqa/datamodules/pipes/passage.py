a421b700cdec9d4bb1e9f06036f26076d7ee9cc2
Andreas Geert Motzfeldt <andreas@motzfeldt.dk>
2022-03-14 16:08:44 +0100

    setup to append doc title to each passage

--- fz_openqa/datamodules/pipes/passage.py
+++ fz_openqa/datamodules/pipes/passage.py
@@ -7,6 +7,9 @@ from typing import Optional
 from typing import Tuple
 from typing import Union
 
+import rich
+
+from ...utils.pretty import pprint_batch
 from .base import Pipe
 from fz_openqa.utils.datastruct import Batch
 
@@ -14,7 +17,7 @@ from fz_openqa.utils.datastruct import Batch
 class GeneratePassages(Pipe):
     """A pipe to Extract passages from a batch of documents."""
 
-    required_keys = ["input_ids", "attention_mask", "offset_mapping"]
+    required_keys = ["document.input_ids", "document.attention_mask", "document.offset_mapping"]
     _allows_update = False
 
     def __init__(
@@ -22,6 +25,7 @@ class GeneratePassages(Pipe):
         *,
         size: int,
         stride: int,
+        append_document_titles: bool = False,
         start_tokens: List[int],
         end_tokens: List[int],
         pad_token_id: int,
@@ -37,6 +41,8 @@ class GeneratePassages(Pipe):
             The size of the passage to extract.
         stride
             The stride of the passage to extract.
+        append_document_titles
+            Whether or not append document titles to each passage
         start_tokens
             The tokens to append to the start of each passage
         end_tokens
@@ -51,24 +57,24 @@ class GeneratePassages(Pipe):
             Other arguments to pass to the base Pipe class.
         """
         super(GeneratePassages, self).__init__(**kwargs)
-
+        self.append_document_titles = append_document_titles
         self.verbose = verbose
         self.global_keys = global_keys
         base_args = {"size": size, "stride": stride}
         self.args = {
-            "input_ids": {
+            "document.input_ids": {
                 "pad_token": pad_token_id,
                 "start_tokens": start_tokens,
                 "end_tokens": end_tokens,
                 **base_args,
             },
-            "attention_mask": {
+            "document.attention_mask": {
                 "pad_token": 0,
                 "start_tokens": [0 for _ in start_tokens],
                 "end_tokens": [0 for _ in end_tokens],
                 **base_args,
             },
-            "offset_mapping": {
+            "document.offset_mapping": {
                 "pad_token": [-1, -1],
                 "start_tokens": [[-1, -1] for _ in start_tokens],
                 "end_tokens": [[-1, -1] for _ in end_tokens],
@@ -83,15 +89,16 @@ class GeneratePassages(Pipe):
         self._check_input_keys(batch)
         indexes, output = self.generate_passages_for_all_keys(
             batch,
-            keys=["input_ids", "attention_mask", "offset_mapping"],
+            keys=["document.input_ids", "document.attention_mask", "document.offset_mapping"],
             args=self.args,
             global_keys=self.global_keys,
+            append_document_titles=self.append_document_titles,
         )
 
         # extract document.text
-        output["text"] = [
-            self.extract_passage_text_from_doc(batch["text"][idx], ofs_ids)
-            for idx, ofs_ids in zip(indexes, output["offset_mapping"])
+        output["document.text"] = [
+            self.extract_passage_text_from_doc(batch["document.text"][idx], ofs_ids)
+            for idx, ofs_ids in zip(indexes, output["document.offset_mapping"])
         ]
 
         return output
@@ -102,12 +109,13 @@ class GeneratePassages(Pipe):
                 f"key={key} must be provided. " f"Found batch.keys={list(batch.keys())}."
             )
 
-    @staticmethod
     def generate_passages_for_all_keys(
+        self,
         examples: Dict[str, List[Any]],
         keys: List[str],
         args: Dict[str, Dict[str, Any]],
         global_keys: Optional[List[str]] = None,
+        append_document_titles: bool = False,
     ) -> Tuple[List[int], Batch]:
         """
         This functions generate the passages for each attribute in `keys`,
@@ -124,7 +132,7 @@ class GeneratePassages(Pipe):
             - indexes: index of the parent example for each passage
             - output: Batch of data for all keys + `idx` (document id) and `passage_mask`
         """
-        assert "idx" in examples.keys()
+        assert "document.idx" in examples.keys()
         assert all(key in args.keys() for key in keys)
         L = len(next(iter(examples.values())))
         assert all(L == len(x) for x in examples.values())
@@ -132,7 +140,13 @@ class GeneratePassages(Pipe):
         first_key, *other_keys = keys
         output = defaultdict(list)
         indexes = []
+
         for idx, example in enumerate(examples[first_key]):
+            if append_document_titles:
+                input_key = first_key.split(".")[-1]
+                args = get_title_tokens(
+                    args=args, arg_key=first_key, temp_input=examples[f"title.{input_key}"][idx]
+                )
 
             global_values = {}
             if global_values is not None:
@@ -153,7 +167,14 @@ class GeneratePassages(Pipe):
 
             # do another pass to generate the passages for each remaining attribute
         for key in other_keys:
-            for example in examples[key]:
+            for idx, example in enumerate(examples[key]):
+                if append_document_titles:
+                    input_key = key.split(".")[-1]
+                    args = get_title_tokens(
+                        args=args,
+                        arg_key=key,
+                        temp_input=examples[f"title.{input_key}"][idx],
+                    )
                 passages = gen_passages(example, **args[key], return_mask=False)
                 for i, passage in enumerate(passages):
                     output[key].append(passage)
@@ -178,6 +199,7 @@ def gen_passages(
     *,
     size: int,
     stride: int,
+    title_tokens: Optional[List[Any]] = None,
     start_tokens: Optional[List[Any]] = None,
     end_tokens: Optional[List[Any]] = None,
     pad_token: Optional[Any] = None,
@@ -200,6 +222,12 @@ def gen_passages(
     else:
         end_tokens = []
 
+    if title_tokens is not None:
+        eff_size -= len(title_tokens)
+        eff_stride -= len(title_tokens)
+    else:
+        title_tokens = []
+
     assert eff_size > 0
     assert eff_stride > 0
     assert eff_stride <= eff_size
@@ -215,8 +243,7 @@ def gen_passages(
         if len(seq) > left_pad:
 
             # define the passage
-            seq = start_tokens + seq + end_tokens + padding * [pad_token]
-
+            seq = start_tokens + title_tokens + seq + end_tokens + padding * [pad_token]
             # define the passage mask
             mask = (
                 (len(start_tokens) + left_pad) * [0]
@@ -233,3 +260,19 @@ def gen_passages(
                 )
             else:
                 yield seq
+
+
+def get_title_tokens(
+    args: dict,
+    arg_key: str,
+    temp_input: List[int],
+) -> dict:
+    if "input_ids" in arg_key:
+        args[arg_key]["title_tokens"] = temp_input
+    elif "attention_mask" in arg_key:
+        args[arg_key]["title_tokens"] = [1 for _ in temp_input]
+    elif "offset_mapping" in arg_key:
+        args[arg_key]["title_tokens"] = [[-1, -1] for _ in temp_input]
+    else:
+        NotImplementedError
+    return args
