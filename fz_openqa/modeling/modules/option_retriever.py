Fix aggregated retriever KL

fz_openqa/modeling/modules/option_retriever.py
@@ -366,7 +366,7 @@ class OptionRetriever(Module):
         match_score: Optional[Tensor] = None,
         document_ids: Optional[Tensor] = None,
         reader_score: Optional[Tensor] = None,
-        log_p_d: Optional[Tensor] = None,
+        agg_retriever_score: Optional[Tensor] = None,
         output: Dict,
     ):
         """
@@ -394,8 +394,10 @@ class OptionRetriever(Module):
         output["retriever/entropy"] = retriever_entropy.mean()
 
         # entropy H(p(d))
-        if log_p_d is not None:
-            retriever_entropy_agg = -(log_p_d * log_p_d).sum(dim=-1)
+        if agg_retriever_score is not None:
+            log_nq = math.log(agg_retriever_score.size(0))
+            log_p_d = (agg_retriever_score.log_softmax(dim=-1) - log_nq).logsumexp(dim=0)
+            retriever_entropy_agg = -(log_p_d.exp() * log_p_d).sum(dim=-1)
             output["retriever/entropy_agg"] = retriever_entropy_agg.mean()
 
         if retrieval_scores is not None: