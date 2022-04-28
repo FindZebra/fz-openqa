c192db61c9db3c17423c4508c6a142fc2a6e3b6b
Valentin Liévin <valentin.lievin@gmail.com>
2022-04-28 09:15:28 +0200

    configs

--- submit_race.sh
+++ submit_race.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=fz-race
 #SBATCH --output=./slurm/%j.out
-#SBATCH --ntasks=1 --cpus-per-task=8 --mem=64G
+#SBATCH --ntasks=1 --cpus-per-task=8 --mem=32G
 #SBATCH -p gpu --gres=gpu:titanrtx:2
 #SBATCH --time=7-00:00:00
 
@@ -22,11 +22,11 @@ then
 fi
 
 # run the model
-poetry run pythono run.py \
+poetry run python run.py \
   +experiment=option_retriever +environ=diku +patch=race \
   base.device_batch_size=4 \
   base.eval_device_batch_size=8 \
-  base.infer_batch_mul=2 \
+  base.infer_batch_mul=100 \
   trainer.precision=32 \
   datamodule.num_workers=8 \
   +kill_es=true \
