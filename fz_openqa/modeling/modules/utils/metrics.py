fdc3d7a8abae5b2ab3dbb36cb82552044c4b0a74
Valentin Liévin <valentin.lievin@gmail.com>
2022-05-07 21:12:11 +0200

    disable retrieval metric (training speed) ; except MRR

--- fz_openqa/modeling/modules/utils/metrics.py
+++ fz_openqa/modeling/modules/utils/metrics.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from copy import copy
 from copy import deepcopy
 from typing import Any
 from typing import Dict
@@ -91,38 +90,43 @@ class SafeMetricCollection(MetricCollection):
 
     def update(self, *args: Any, **kwargs: Any) -> None:
         args = list(args)
+
+        # automatically create the index if needed
+        if len(args) < 3 and any(isinstance(m, RetrievalMetric) for m in self.values()):
+            m = [m for m in self.values() if isinstance(m, RetrievalMetric)][0]
+            preds, targets = args
+            start_index = 1 + max(i.max() for i in m.indexes) if len(m.indexes) else 0
+            indexes = torch.arange(start_index, len(preds) + start_index, device=preds.device)
+            indexes = indexes.view(indexes.size(0), *(1 for _ in preds.shape[1:]))
+            indexes = indexes.expand_as(preds)
+            args = [preds, targets, indexes]
+
         for _, m in self.items(keep_base=True):
-            m_args = copy(args)
 
             # handle Accuracy
             if isinstance(m, Accuracy):
                 # skip metric if n_documents <= k
-                preds, targets, *_ = m_args
+                preds, targets, *_ = args
                 if m.top_k is not None and preds.shape[-1] <= m.top_k:
                     continue
+
+                m_kwargs = m._filter_kwargs(**kwargs)
+                m.update(*args[:2], **m_kwargs)
 
             # handle Retrieval Metrics
             elif isinstance(m, RetrievalMetric):
                 # skip metric if n_documents <= k
                 if hasattr(m, "k"):
-                    preds, targets, *_ = m_args
+                    preds, targets, *_ = args
                     if m.k is not None and preds.shape[-1] <= m.k:
                         continue
 
-                # automatically create the index if needed
-                if len(m_args) < 3:
-                    preds, targets = m_args
-                    start_index = 1 + max(i.max() for i in m.indexes) if len(m.indexes) else 0
-                    indexes = torch.arange(
-                        start_index, len(preds) + start_index, device=preds.device
-                    )
-                    indexes = indexes.view(indexes.size(0), *(1 for _ in preds.shape[1:]))
-                    indexes = indexes.expand_as(preds)
-                    m_args = [preds, targets, indexes]
+                m_kwargs = m._filter_kwargs(**kwargs)
+                m.update(*args[:3], **m_kwargs)
 
-            # finally update the metric
-            m_kwargs = m._filter_kwargs(**kwargs)
-            m.update(*m_args, **m_kwargs)
+            else:
+                m_kwargs = m._filter_kwargs(**kwargs)
+                m.update(*args, **m_kwargs)
 
     def compute(self) -> Dict[str, Any]:
         return {k: m.compute() for k, m in self.items() if is_computable(m)}
