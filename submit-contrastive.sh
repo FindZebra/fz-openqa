30dbbb18899f6db31ca74d92605b992f8f254701
Valentin Lievin <valentin.lievin@gmail.com>
2022-05-19 19:12:24 +0200

    fix config

--- submit-contrastive.sh
+++ submit-contrastive.sh
@@ -26,7 +26,7 @@ poetry run python run.py +experiment=contrastive +environ=diku \
   +patch=dpr \
   base.device_batch_size=1 \
   base.infer_batch_mul=10 \
-  base.datamodule.dset_name=medqa-tw \
+  datamodule.dset_name=medqa-tw \
   base.eval_device_batch_size=2 \
   trainer.precision=32 \
   datamodule.num_workers=8 \
